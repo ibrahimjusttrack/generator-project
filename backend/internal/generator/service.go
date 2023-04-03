df4cbf4b75fefb4260b47ea1e318c87fc1ff92be
Gleb Nazemnov <gleb@justtrack.io>
2023-04-03 16:13:15 +0200

    add interface

--- backend/internal/generator/service.go
+++ backend/internal/generator/service.go
@@ -1,4 +1,11 @@
 package generator
 
+type Input struct {
+	Key   string
+	Value string
+}
+
 type Service interface {
+	//return path for file and error of generator
+	Generate(templateId string, input []Input) (string, error)
 }
