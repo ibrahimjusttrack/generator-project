7696870e767933f4ba1b5837d9d514dfd9162343
Naman Balaji <naman@justtrack.io>
2023-04-03 15:53:13 +0200

    minor fix

--- backend/internal/routes/routes.go
+++ backend/internal/routes/routes.go
@@ -2,11 +2,16 @@ package routes
 
 import (
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"myapp/internal/handlers"
 )
 
 func New() *echo.Echo {
 	e := echo.New()
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"http://localhost:3000"},
+		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
+	}))
 
 	e.GET("/", handlers.Hello)
 	e.GET("/templates/all", handlers.GetAllTemplates)
